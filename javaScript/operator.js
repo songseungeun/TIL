853feace56909fbcef242ce7a68f9f37df1b5f52
songseungeun <grayandjj@gmail.com>
2020-01-26 21:46:56 +0900

    study java script 'operator'

---
+++ javaScript/operator.js
@@ -0,0 +1,50 @@
+let a = 1 + 2;
+console.log(a);
+
+let b = 1;
+console.log(b++);
+console.log(b);
+console.log(++b);
+
+let c = 1;
+console.log(c--);
+console.log(c);
+console.log(a + c);
+
+// a++ 보여주고 계산하는 것
+// ++a 계산하고 보여주는 것
+
+let d = 2;
+console.log(d + b);
+
+// e 와 f 는 같은 연산
+let e = 1;
+e = e + 3;
+console.log(e);
+
+let f = 1;
+f += 3;
+console.log(f);
+
+let g = 3;
+g -= 2;
+console.log(g);
+
+let h = 2;
+h *= 3;
+console.log(h);
+
+let i = 6;
+i /= 2;
+console.log(i);
+// 사칙연산 연습
+
+// 논리연산자
+// AND 연산자 = !
+let j = !true;
+console.log(j);
+
+let k = !false;
+console.log(k);
+
+// AND 연산자
