cd750d4e408cfad5eeb7cd79ddf193aedb9d66b0
songseungeun <grayandjj@gmail.com>
2020-02-17 16:18:00 +0900

    표현식 공부

--- javaScript/expression.js
+++ javaScript/expression.js
@@ -50,3 +50,18 @@ var score3 = 100;
 var score4 = 50 + 50;
 score3;
 
+
+// 변수 선언문
+var x;
+
+// 표현식 문 (할당문)
+x = 5;
+
+// 함수 선언문
+function foo () {}
+
+// 조건문
+if (x > 1) { console.log(x); }
+
+// 반복문
+for (var i = 0; i < 2; i++) { console.log(i); }
