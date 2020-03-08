1a6c207ece0c3f4668866c2e4675e9312377cca9
songseungeun <grayandjj@gmail.com>
2020-03-08 23:57:18 +0900

    코드 수정 및 추가

--- javaScript/exercise-control-flow.js
+++ javaScript/exercise-control-flow.js
@@ -10,6 +10,12 @@ if (20 > x && x > 10) {
 // 2. for문을 사용하여 0부터 10미만의 정수 중에서 짝수만을 작은 수부터 출력하시오.
 
 for (var i = 0; i < 10; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+for (var i = 0; i < 10; i++) {
   if (!(i % 2)) {
     console.log(i);
   }
@@ -21,6 +27,13 @@ for (let i = 0; i < 10; i += 2) {
 }
 
 // 3. for문을 사용하여 0부터 10미만의 정수 중에서 짝수만을 작은 수부터 문자열로 출력하시오.
+var even = "";
+for (var i = 0; i < 10; i++) {
+  if (i % 2 === 0) {
+    even += `${i}`;
+  }
+}
+console.log(even);
 
 var even = '';
 for (var i = 0; i < 10; i++) {
@@ -51,6 +64,13 @@ for (let i = 9; i > 0; i -= 2) {
 }
 
 // 5. while문을 사용하여 0 부터 10 미만의 정수 중에서 짝수만을 작은 수부터 출력하시오.
+var a = 0;
+while (a < 10) {
+  if (a % 2 === 0) {
+    console.log(a);
+  }
+  a++;
+}
 
 var a = 0;
 while (a < 10) {
@@ -234,6 +254,20 @@ for (let i = 0; i < 5; i++) {
 
 console.log(result);
 
+// 삼항 조건 연산자
+
+var star = "*";
+var newLine = "\n";
+var result = "";
+
+for (var i = 0; i < 5; i++) {
+  for (var j = 0; j < 6; j++) {
+    result += i > j ? " " : j === 5 ? newLine : star;
+  }
+}
+
+console.log(result);
+
 // if...else
 var star = '*';
 var newLine = '\n';
@@ -270,7 +304,7 @@ for (let i = 5; i > 0; i--) {
 
 console.log(result);
 
-// for 마지막 개행 없애기
+// for break
 let star = "*";
 let newLine = "\n";
 let result = "";
@@ -281,6 +315,19 @@ for (let i = 5; i > 0; i--) {
   }
   if (i === 1) break;
   result += newLine;
+}
+
+console.log(result);
+
+// 삼항 조건 연산자
+var star = "*";
+var newLine = "\n";
+var result = "";
+
+for (var i = 5; i > 0; i--) {
+  for (var j = 0; j < i + 1; j++) {
+    result += i > j ? star : newLine;
+  }
 }
 
 console.log(result);
@@ -304,24 +351,7 @@ console.log(result);
 
 // 14. 삼각형 출력하기 - pattern 4
 
-// for
-let star = "*";
-let newLine = "\n";
-let result = "";
-
-for (let i = 0; i < 5; i++) {
-  for (let j = 4; j > i; j--) {
-    result += " ";
-  }
-  for (let k = 0; k <= i; k++) {
-    result += star;
-  }
-  result += newLine;
-}
-
-console.log(result);
-
-// 마지막 개행 없애기
+// for break
 let star = "*";
 let newLine = "\n";
 let result = "";
@@ -335,6 +365,19 @@ for (let i = 0; i < 5; i++) {
   }
   if (i === 4) break;
   result += newLine;
+}
+
+console.log(result);
+
+// 삼항 조건 연산자
+var star = "*";
+var result = "";
+var newLine = "\n";
+
+for (var i = 0; i < 5; i++) {
+  for (var j = 0; j < 6; j++) {
+    result += 4 - i > j ? " " : j === 5 ? newLine : star;
+  }
 }
 
 console.log(result);
@@ -360,24 +403,6 @@ console.log(result);
 
 // 15. 정삼각형 출력하기
 
-var star = '*';
-var result = '';
-var newLine = '\n';
-
-for (var i = 0; i < 5; i++) {
-  for (var j = 0; j < i + 6; j++) {
-    if (i - 4 < -j) {
-      result += ' ';
-    } else if (j === i + 5) {
-      result += newLine;
-    } else {
-      result += star;
-    }
-  }
-}
-
-console.log(result);
-
 // for, break
 var star = "*";
 var result = "";
@@ -396,27 +421,42 @@ for (let i = 0; i < 5; i++) {
 
 console.log(result);
 
+// 삼항 조건 연산자
+var star = "*";
+var result = "";
+var newLine = "\n";
 
-// 16. 역정삼각형 출력하기
+for (var i = 0; i < 5; i++) {
+  for (var j = 0; j < i + 6; j++) {
+    result += i - 4 < -j ? " " : j === i + 5 ? newLine : star;
+  }
+}
 
+console.log(result);
+
+// if
 var star = '*';
 var result = '';
 var newLine = '\n';
 
-for (var i = 5; i > 0; i--) {
-  for (var j = 0; j < i + 5; j++) {
-    if (i - 5 < -j) {
+for (var i = 0; i < 5; i++) {
+  for (var j = 0; j < i + 6; j++) {
+    if (i - 4 < -j) {
       result += ' ';
-    } else if (j === i + 4) {
+    } else if (j === i + 5) {
       result += newLine;
     } else {
       result += star;
     }
   }
 }
+
 console.log(result);
 
-//
+
+// 16. 역정삼각형 출력하기
+
+// for, break
 var star = "*";
 var result = "";
 var newLine = "\n";
@@ -432,4 +472,34 @@ for (let i = 0; i < 5; i++) {
   result += newLine;
 }
 
+console.log(result);
+
+// 삼항 조건 연산자
+var star = "*";
+var result = "";
+var newLine = "\n";
+
+for (var i = 5; i > 0; i--) {
+  for (var j = 0; j < i + 5; j++) {
+    result += i - 5 < -j ? " " : j === i + 4 ? newLine : star;
+  }
+}
+console.log(result);
+
+// if
+var star = '*';
+var result = '';
+var newLine = '\n';
+
+for (var i = 5; i > 0; i--) {
+  for (var j = 0; j < i + 5; j++) {
+    if (i - 5 < -j) {
+      result += ' ';
+    } else if (j === i + 4) {
+      result += newLine;
+    } else {
+      result += star;
+    }
+  }
+}
 console.log(result);
