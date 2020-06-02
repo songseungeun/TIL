a5ff0221a86f46542903cff99acc2f527abc8e67
songseungeun <grayandjj@gmail.com>
2020-06-02 17:42:29 +0900

    add custom hook

--- react/address-book/src/App.js
+++ react/address-book/src/App.js
@@ -1,13 +1,8 @@
-import React, {
-  useState,
-  useRef,
-  useMemo,
-  useCallback,
-  useReducer,
-} from "react";
+import React, { useRef, useMemo, useCallback, useReducer } from "react";
 import { createGlobalStyle } from "styled-components";
 import CreateUser from "./CreateUser";
 import UserList from "./UserList";
+import useInputs from "./useInputs";
 
 const GlobalStyle = createGlobalStyle`
   body {
@@ -42,28 +37,8 @@ const GlobalStyle = createGlobalStyle`
 `;
 
 function countActiveUsers(users) {
-  console.log("즐겨찾기 사용자 수를 세는 중...");
+  // console.log("즐겨찾기 사용자 수를 세는 중...");
   return users.filter((user) => user.active).length;
-}
-
-function reducer(state, action) {
-  switch (action.type) {
-    case "CHANGE_INPUT":
-      return {
-        ...state,
-        inputs: {
-          ...state.inputs,
-          [action.name]: action.value,
-        },
-      };
-    case "CREATE_USERS":
-      return {
-        inputs: initialState.inputs,
-        users: [...state.users, action.user],
-      };
-    default:
-      throw new Error("Unhandled action");
-  }
 }
 
 const initialState = {
@@ -81,72 +56,77 @@ const initialState = {
   ],
 };
 
+function reducer(state, action) {
+  switch (action.type) {
+    case "CHANGE_INPUT":
+      return {
+        ...state,
+        inputs: {
+          ...state.inputs,
+          [action.name]: action.value,
+        },
+      };
+    case "CREATE_USER":
+      return {
+        inputs: initialState.inputs,
+        users: [...state.users, action.user],
+      };
+    case "TOGGLE_USER":
+      return {
+        ...state,
+        users: state.users.map((user) =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        ),
+      };
+    case "REMOVE_USER":
+      // return;
+      return {
+        ...state,
+        users: state.users.filter((user) => user.id !== action.id),
+      };
+    default:
+      throw new Error("Unhandled action");
+  }
+}
+
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [form, onChange, reset] = useInputs({
+    username: "",
+    email: "",
+  });
+  const { username, email } = form;
+  const nextId = useRef(4);
   const { users } = state;
-  const { username, email } = state.inputs;
-  // console.log(state);
-
-  const onChange = useCallback((e) => {
-    const { name, value } = e.target;
-    dispatch({
-      type: "CHANGE_INPUT",
-      name,
-      value,
-    });
-  }, []);
 
   const onCreate = useCallback(() => {
     dispatch({
-      type: "CREATE_USERS",
+      type: "CREATE_USER",
       user: {
-        id: 4,
+        id: nextId.current,
         username,
         email,
       },
     });
-  }, [username, email]);
+    nextId.current += 1;
+    reset();
+  }, [username, email, reset]);
 
-  // console.log(onCreate);
-  // console.log(username);
+  const onToggle = useCallback((id) => {
+    dispatch({
+      type: "TOGGLE_USER",
+      id,
+    });
+  }, []);
 
-  // const [inputs, setInputs] = useState();
+  const removeUser = useCallback((id) => {
+    dispatch({
+      type: "REMOVE_USER",
+      id,
+    });
+  }, []);
 
-  // const { username, email } = inputs;
-
-  // const onChange = useCallback(
-  //   ({ target }) => {
-  //     const { value, name } = target;
-  //     setInputs({ ...inputs, [name]: value });
-  //   },
-  //   [inputs]
-  // );
-
-  // const createId = useRef(4);
-
-  //   createId.current += 1;
-  // }, [username, email, users]);
-
-  // const onToggle = useCallback(
-  //   (id) => {
-  //     setUsers(
-  //       users.map((user) =>
-  //         user.id === id ? { ...user, active: !user.active } : user
-  //       )
-  //     );
-  //     console.log(users);
-  //   },
-  //   [users]
-  // );
-
-  // const removeUser = useCallback(
-  //   (id) => {
-  //     setUsers(users.filter((user) => user.id !== id));
-  //   },
-  //   [users]
-  // );
-
-  // const count = useMemo(() => countActiveUsers(users), [users]);
+  const count = useMemo(() => countActiveUsers(users), [users]);
 
   return (
     <>
@@ -160,9 +140,9 @@ function App() {
           onCreate={onCreate}
         />
         <div className="fav-length">
-          즐겨찾기 한 사람은 <strong>0명</strong> 입니다.
+          즐겨찾기 한 사람은 <strong>{count}명</strong> 입니다.
         </div>
-        <UserList users={users} />
+        <UserList users={users} onToggle={onToggle} removeUser={removeUser} />
       </div>
     </>
   );
