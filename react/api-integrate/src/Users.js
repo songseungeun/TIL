32cd8dc4db523e98c4b95d1a0df69fc073a62c43
songseungeun <grayandjj@gmail.com>
2020-06-05 17:32:43 +0900

    add context api

---
+++ react/api-integrate/src/Users.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import User from "./User";
+import { useUsersState, useUsersDispatch, getUsers } from "./UsersContext";
+
+function Users() {
+  const [userId, setUserId] = useState(null);
+  const state = useUsersState();
+  const dispatch = useUsersDispatch();
+
+  const { loading, data: users, error } = state.users;
+
+  const fetchData = () => {
+    getUsers(dispatch);
+  };
+
+  if (loading) return <div>로딩중입니다.</div>;
+  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!users) return <button onClick={fetchData}>불러오기</button>;
+
+  return (
+    <>
+      <ul>
+        {users.map((user) => (
+          <li
+            id={user.id}
+            key={user.id}
+            onClick={() => setUserId(user.id)}
+            style={{ cursor: "pointer" }}
+          >
+            {user.username} ({user.name})
+          </li>
+        ))}
+      </ul>
+      <button onClick={fetchData}>다시 불러오기</button>
+      {userId && <User id={userId} />}
+    </>
+  );
+}
+
+export default Users;
