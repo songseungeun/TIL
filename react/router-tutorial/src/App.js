51ae02ad0d9783a7fa73f081cb949aeb8d317e2d
songseungeun <grayandjj@gmail.com>
2020-06-06 20:18:16 +0900

    study router

---
+++ react/router-tutorial/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Profile from "./Profile";
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route path="/" component={Home} exact />
+      <Route path="/about" component={About} />
+      <Route path="/profiles/:username" component={Profile} />
+    </div>
+  );
+}
+
+export default App;
