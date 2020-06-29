4dcccb81f6d1c8982ed1e0f4d7fb5eefb58f387e
Song Seungeun <grayandjj@gmail.com>
2020-06-29 21:35:35 +0900

    learn redux middleware

---
+++ react/learn-redux-middleware/src/lib/asyncUtils.js
@@ -0,0 +1,64 @@
+export const createPromiseThunk = (type, promiseCreator) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  // Flux Standard Action
+  return (param) => async (dispatch) => {
+    dispatch({ type, param });
+    try {
+      const payload = await promiseCreator(param);
+      dispatch({ type: SUCCESS, payload });
+    } catch (e) {
+      dispatch({ type: ERROR, payload: e, error: true });
+    }
+  };
+};
+
+export const handlerAsyncActions = (type, key) => {
+  const [SUCCESS, ERROR] = [`${type}_SUCCESS`, `${type}_ERROR`];
+
+  // reducer update
+  return (state, action) => {
+    switch (action.type) {
+      case type:
+        return {
+          ...state,
+          [key]: reducerUtils.loading(),
+        };
+      case SUCCESS:
+        return {
+          ...state,
+          [key]: reducerUtils.success(action.payload),
+        };
+      case ERROR:
+        return {
+          ...state,
+          [key]: reducerUtils.error(action.payload),
+        };
+      default:
+        return state;
+    }
+  };
+};
+
+export const reducerUtils = {
+  initial: (data = null) => ({
+    data,
+    loading: false,
+    error: null,
+  }),
+  loading: (prevState = null) => ({
+    loading: true,
+    data: prevState,
+    error: null,
+  }),
+  success: (payload) => ({
+    loading: false,
+    data: payload,
+    error: null,
+  }),
+  error: (error) => ({
+    loading: false,
+    data: null,
+    error,
+  }),
+};
---
+++ react/learn-redux-middleware/src/modules/posts.js
@@ -0,0 +1,42 @@
+import * as postsAPI from "../api/posts";
+import {
+  reducerUtils,
+  createPromiseThunk,
+  handlerAsyncActions,
+} from "../lib/asyncUtils";
+
+const GET_POSTS = "GET_POSTS";
+const GET_POSTS_SUCCESS = "GET_POSTS_SUCCESS";
+const GET_POSTS_ERROR = "GET_POSTS_ERROR";
+
+const GET_POST = "GET_POST";
+const GET_POST_SUCCESS = "GET_POST_SUCCESS";
+const GET_POST_ERROR = "GET_POST_ERROR";
+
+export const getPosts = createPromiseThunk(GET_POSTS, postsAPI.getPosts);
+export const getPost = createPromiseThunk(GET_POST, postsAPI.getPostById);
+
+const initialState = {
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial(),
+};
+
+const getPostsReducer = handlerAsyncActions(GET_POSTS, "posts");
+const getPostReducer = handlerAsyncActions(GET_POST, "post");
+
+export default function posts(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+    case GET_POSTS_SUCCESS:
+    case GET_POSTS_ERROR:
+      return getPostsReducer(state, action);
+
+    case GET_POST:
+    case GET_POST_SUCCESS:
+    case GET_POST_ERROR:
+      return getPostReducer(state, action);
+
+    default:
+      return state;
+  }
+}
