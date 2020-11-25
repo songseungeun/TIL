56f97ca3e751e8923f874ef90fe09864de8030c9
Song Seungeun <grayandjj@gmail.com>
2020-11-25 21:25:04 +0900

    study currying func

--- javaScript/currying.js
+++ javaScript/currying.js
@@ -1,5 +1,8 @@
-const outerFunc = (x, y) => (z) => {
-  return x + y + z;
-};
+let outerFunc = (x) => (y) => (z) => x + y + z;
 
-console.log(outerFunc(2, 3)(5));
+// console.log(outerFunc(2, 3)(5));
+
+let plusX = outerFunc(2);
+let plusY = plusX(3);
+
+console.log(plusY(5));
